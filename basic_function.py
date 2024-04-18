ed195faca4489317bd9f143b5000aefd58c5dc72
arhemanthkumar <hemanthar95@gmail.com>
2024-04-18 22:31:53 +0530

    basic layout of or backbone structure of Mouse Jiggler

--- basic_function.py
+++ basic_function.py
@@ -1,28 +1,18 @@
+import sys
 import time
 
 import pyautogui
 print(pyautogui.size())
 
-time.sleep(5)
-pyautogui.moveTo(1000,300,duration=1) #moves in number of pixels in x and y direction along with duration
-# pyautogui.moveRel(500,500,duration=1) #relative mouse movement from the position
-print(pyautogui.position())
+enable_jiggle = True
+mouse_time = 5
 
-# pyautogui.click(80,30,duration=1)
-# pyautogui.click(80,70,duration=0.5)
-
-pyautogui.scroll(-500)
-time.sleep(1)
-pyautogui.scroll(500)
-
-
-pyautogui.click(1000,110,duration=1)
-pyautogui.typewrite("iPhone")
-pyautogui.click(1380,110,duration=1)
-pyautogui.click(1000,110,duration=1)
-pyautogui.hotkey("ctrl","a")
-pyautogui.hotkey("ctrl","c")
-
-pyautogui.hotkey("ctrl","t")
-pyautogui.hotkey("ctrl","v")
-pyautogui.click()
+while enable_jiggle:
+    try:
+        time.sleep(mouse_time)
+        pyautogui.moveRel(1,1)
+        time.sleep(mouse_time)
+        pyautogui.moveRel(-1,-1)
+    except KeyboardInterrupt:
+        # enable_jiggle = False
+        pass
