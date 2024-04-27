321b7f0f3cf3f32db472a322bf3233702c7b94d8
arhemanthkumar <hemanthar95@gmail.com>
2024-04-27 17:20:12 +0530

    testing with updated modern UI

--- Immediate testing.py
+++ Immediate testing.py
@@ -1,51 +1,47 @@
 from tkinter import *
-import sys
 import time
 import pyautogui
 import threading
-root = Tk()
+import customtkinter
+from PIL import Image, ImageTk
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+root = customtkinter.CTk()
+
 
 checkbutton_val = BooleanVar()
 slider_val = IntVar()
-def enablejiggle():
-    if checkbutton_val.get() == False:
-        slider.set(0)
-    if checkbutton_val.get() == True:
-        slider.set(10)
-        # threading.Thread(target=jiggle, daemon=True).start()
-    mouse_time = slider.get()
-    # print(mouse_time)
 
-# def jiggling():
-#     time.sleep(mouse_time)
-#     pyautogui.moveRel(1, 1)
-#     time.sleep(mouse_time)
-#     pyautogui.moveRel(-1, -1)
+
+def enablejiggle():
+    if checkbutton_val.get() is False:
+        slider.set(0)
+    if checkbutton_val.get() is True:
+        slider.set(10)
+
 
 def sliderfunction(value):
 
-   if slider_val.get() == 0:
-       checkbutton_val.set(False)
-   else:
-       checkbutton_val.set(True)
-       threading.Thread(target=jiggle, daemon=True).start()
-   mouse_time = slider.get()
-   print(mouse_time)
+    if slider_val.get() == 0:
+        checkbutton_val.set(False)
+    else:
+        checkbutton_val.set(True)
+        threading.Thread(target=jiggle, daemon=True).start()
+    '''mouse_time = slider.get()
+    print(mouse_time)
+    used for debugging only -> to check immediate slider value
+    '''
 
 
 def jiggle():
-
     count = 0
     mouse_time = slider.get()
-    print("mouse time inside jiggle ",mouse_time)
     while mouse_time > 0:
-
-        print('inside while loop')
-
         if count == mouse_time:
-            pyautogui.moveRel(10, 10)
-            print("delay")
-            pyautogui.moveRel(-10, -10)
+            pyautogui.moveRel(1, 1)
+            pyautogui.moveRel(-1, -1)
             count = 0
         count += 1
         time.sleep(1)
@@ -55,22 +51,31 @@ def jiggle():
             mouse_time = new_mouse_time
 
 
+# creating a label or field widget using grid method
+mylabel1 = customtkinter.CTkLabel(root, text="Enable Jiggle ?", anchor="w",font=('Times', 20))
+checkbox1 = customtkinter.CTkCheckBox(root, text="", command=enablejiggle, variable=checkbutton_val)
 
-#creating a label or field widget using grid method
-mylabel1 = Label(root, text="Enable Jiggle ?", pady=10)
-checkbox1 = Checkbutton(root, padx=50, command=enablejiggle, variable=checkbutton_val)
-
-mylabel1.grid(row = 0, column = 0)
-checkbox1.grid(row = 0, column = 1)
+mylabel1.grid(row=1, column=0,  padx = 15, pady=10, columnspan=2)
+checkbox1.grid(row=1, column=2, padx=100, pady=10, sticky="ew", columnspan=2)
 
 # integrating slider in the bottom
-slider = Scale(root, from_=0, to=60, orient=HORIZONTAL, sliderlength=15, width=15, tickinterval=10, length=270, resolution= 10, command = sliderfunction, variable=slider_val)
-slider.grid(row=1,columnspan=2)
+slider = customtkinter.CTkSlider(root, from_=0, to=60, width=250,  number_of_steps=6,
+                command=sliderfunction, variable=slider_val)
+slider.grid(row=2, columnspan=3, pady=10)
 
-seconds = Label(root, text="seconds")
-seconds.grid(columnspan=2, row=2)
+mylabel2 = customtkinter.CTkLabel(root, text=" 0   10    20    30    40    50    60", anchor="center",font=('Times', 20))
+mylabel2.grid(row=3, column=0,  columnspan=3)
 
-root.geometry("300x150")
+seconds = customtkinter.CTkLabel(root, text="seconds", font=('Times', 20), anchor="center")
+seconds.grid(row = 4,columnspan = 3, pady=5)
+
+
+
+ico = Image.open('icon.ico')
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+
+root.geometry("310x150")
 root.title("Mouse Jiggler")
 root.resizable(False, False)
 root.mainloop()
