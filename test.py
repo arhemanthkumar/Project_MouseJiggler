50f185aa9f4451cca3a82e5cb9f2bb41577c8de9
arhemanthkumar <hemanthar95@gmail.com>
2024-04-30 22:07:16 +0530

    few more changes

--- test.py
+++ test.py
@@ -52,22 +52,25 @@ def jiggle():
 
 
 # creating a label or field widget using grid method
-mylabel1 = customtkinter.CTkLabel(root, text="Enable Jiggle ?", anchor="w",font=('Times', 15))
+mylabel1 = customtkinter.CTkLabel(root, text="Enable Jiggle ?", font=('Times', 15))
 checkbox1 = customtkinter.CTkCheckBox(root, text="", command=enablejiggle, variable=checkbutton_val)
 
-mylabel1.grid(row=1, column=0,  padx = 15, pady=10, columnspan=2)
-checkbox1.grid(row=1, column=2, padx=100, pady=10, sticky="ew", columnspan=2)
+# top_frame=customtkinter.CTkFrame(master=root)
+# top_frame.pack(side="top", fill="both")
+
+mylabel1.place(relx=0.1, rely=0.05)
+checkbox1.place(relx = 0.8,rely=0.05)
 
 # integrating slider in the bottom
-slider = customtkinter.CTkSlider(root, from_=0, to=60, width=250,  number_of_steps=6,
+slider = customtkinter.CTkSlider(root, from_=0, to=60,  number_of_steps=6,
                 command=sliderfunction, variable=slider_val)
-slider.grid(row=2, columnspan=3, pady=10)
+slider.place(relx = 0.1,rely=0.4)
 
-mylabel2 = customtkinter.CTkLabel(root, text=" 0   10    20    30    40    50    60", anchor="center",font=('Times', 15))
-mylabel2.grid(row=3, column=0,  columnspan=3)
+mylabel2 = customtkinter.CTkLabel(root, text="  0   10    20    30    40    50    60", anchor="center",font=('Times', 15))
+mylabel2.place(relx=0.1,rely=0.55)
 
 seconds = customtkinter.CTkLabel(root, text="seconds", font=('Times', 15), anchor="center")
-seconds.grid(row = 4,columnspan = 3)
+seconds.place(relx=0.4,rely=0.75)
 
 
 
@@ -75,7 +78,7 @@ ico = Image.open('icon.ico')
 photo = ImageTk.PhotoImage(ico)
 root.wm_iconphoto(False, photo)
 
-root.geometry("250x150")
+root.geometry("250x120")
 root.title("Mouse Jiggler")
 root.resizable(False, False)
 root.mainloop()
